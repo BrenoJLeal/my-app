44ebe21fd5c17a5bbc3532e21b00ea8b5c2d34b3
Breno Leal <brenoooleal@gmail.com>
2024-01-08 15:40:14 -0300

    Updates

---
+++ precache-manifest.89a8c9b0e1b48f98b9c569f693663ede.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a54a0f6cad0f86005420c6217e661f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/my-app/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/my-app/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/my-app/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "96e662d8dfcec3759e8c",
+    "url": "/my-app/static/js/main.01b01939.chunk.js"
+  },
+  {
+    "revision": "963c8cf830fccb149c29",
+    "url": "/my-app/static/js/runtime~main.e92001b1.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-app/static/media/logo.06e73328.svg"
+  }
+]);
